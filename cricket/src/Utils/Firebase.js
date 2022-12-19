f3130f223004b098d540c12cae9f891963dce07e
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-12-19 13:04:04 +0530

    add .env file

--- cricket/src/Utils/Firebase.js
+++ cricket/src/Utils/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCioz7-9V3CGpxbMpQoDG2KBHFUjN2e05c",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: "cricket-4bc52.firebaseapp.com",
   projectId: "cricket-4bc52",
   storageBucket: "cricket-4bc52.appspot.com",
-  messagingSenderId: "437352731465",
-  appId: "1:437352731465:web:db227cbe7142406a510098",
+  messagingSenderId: `${process.env.REACT_APP_MESSAGE_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_ID}`,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
